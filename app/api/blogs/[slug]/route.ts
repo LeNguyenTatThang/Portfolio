6b455019c51996833496a438a66edf0a5c1db35d
le Thang <lenguyentatthang.dev@gmail.com>
2025-11-30 03:08:29 +0700

    fix api get blog slug

--- app/api/blogs/[slug]/route.ts
+++ app/api/blogs/[slug]/route.ts
@@ -1,19 +1,13 @@
 import { NextResponse, NextRequest } from "next/server"
 import { getBlogsDataBySlug } from "@/services/blogs"
 
-export const GET = async (
-    req: NextRequest,
-    { params }: { params: { slug: string } }
-) => {
-    try {
-        const { slug } = params
-        const data = await getBlogsDataBySlug(slug)
-        
-        return NextResponse.json(data, { status: 200 })
-    } catch (error) {
-        return NextResponse.json(
-            { message: "Internal Server Error" },
-            { status: 500 }
-        )
-    }
+export const GET = async (req: NextRequest, context: { params: { slug: string } }) => {
+  try {
+    const { slug } = context.params
+    const data = await getBlogsDataBySlug(slug)
+
+    return NextResponse.json(data, { status: 200 })
+  } catch {
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 })
+  }
 }
